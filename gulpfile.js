7917227649b64a8ca7330f6a94247aba767a3891
cgaulin <cgaulin@hartford.edu>
2021-04-13 00:52:29 -0400

    Added HTML Validator task

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const { src } = require(`gulp`);
+const htmlValidator = require(`gulp-html`);
+
+let validateHTML = () => {
+    return src([
+        `html/*.html`,
+        `html/**/*.html`])
+        .pipe(htmlValidator());
+};
+
+exports.validateHTML = validateHTML;
