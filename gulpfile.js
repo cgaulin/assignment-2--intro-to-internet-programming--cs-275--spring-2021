1e8ccc0d8a94b255818754b5779a06387dd9a361
cgaulin <cgaulin@hartford.edu>
2021-04-16 15:16:23 -0400

    Added all necessary functions to gulpfile.js from template

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,43 @@
-const { src } = require(`gulp`);
+const { src, dest, series, watch } = require(`gulp`);
+const babel = require(`gulp-babel`);
+const htmlCompressor = require(`gulp-htmlmin`);
 const htmlValidator = require(`gulp-html`);
+const cssLinter = require(`gulp-stylelint`);
+const jsLinter = require(`gulp-eslint`);
+const jsCompressor = require(`gulp-uglify`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
+
+
+async function safari () {
+    browserChoice = `safari`;
+}
+
+async function firefox () {
+    browserChoice = `firefox`;
+}
+
+async function chrome () {
+    browserChoice = `google chrome`;
+}
+
+async function opera () {
+    browserChoice = `opera`;
+}
+
+async function edge () {
+    browserChoice = `microsoft-edge`;
+}
+
+async function allBrowsers () {
+    browserChoice = [
+        `safari`,
+        `firefox`,
+        `google chrome`,
+        `opera`,
+        `microsoft-edge`
+    ];
+}
 
 let validateHTML = () => {
     return src([
@@ -8,4 +46,94 @@ let validateHTML = () => {
         .pipe(htmlValidator());
 };
 
+let compressHTML = () => {
+    return src([`html/*.html`,`html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let compileCSSForProd = () => {
+    return src(`dev/css/style.css`)
+        .pipe(sass({
+            outputStyle: `compressed`,
+            precision: 10
+        }).on(`error`, sass.logError))
+        .pipe(dest(`prod/css`));
+};
+
+let transpileJSForProd = () => {
+    return src(`dev/js/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let lintCSS = () => {
+    return src(`css/*.css`)
+        .pipe(cssLinter({
+            failAfterError: true,
+            reproters: [
+                {formatter: `verbose`, console: true}
+            ]
+        }));
+};
+
+let lintJS = () => {
+    return src(`dev/js/*.js`)
+        .pipe(jsLinter({
+            parserOptions: {
+                ecmaVersion: 2017,
+                sourceType: `module`
+            },
+            rules: {
+                indent: [2, 4, {SwitchCase: 1}],
+                quotes: [2, `backtick`],
+                semi: [2, `always`],
+                'linebreak-style': [2, `unix`],
+                'max-len': [1, 85, 4]
+            },
+            env: {
+                es6: true,
+                node: true,
+                browser: true
+            },
+            extends: `eslint:recommended`
+        }))
+        .pipe(jsLinter.formatEach(`compact`, process.stderr));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        port: 9000,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `dev`
+            ]
+        }
+    });
+
+    watch(`dev/js/*.js`,
+        series(lintJS, transpileJSForDev)
+    ).on(`change`, reload);
+
+    watch(`dev/css/**/*.css`,
+        series(compileCSSForDev)
+    ).on(`change`, reload);
+  watch(`dev/**/*.html`,
+        series(validateHTML)
+    ).on(`change`, reload);
+
+};
+
 exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.lintJS = lintJS;
+exports.build = series(
+    compressHTML,
+    compileCSSForProd,
+    transpileJSForProd,
+);
+exports.serve = series(validateHTML, compressHTML, lintCSS, lintJS, serve);
